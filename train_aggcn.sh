04d6574a0d5b1f04660444fc2b8da6b5e3ce466b
asmaulhusnabd <tithi_cse_ruet@yahoo.com>
2022-04-13 22:55:44 -0600

    Update train_aggcn.sh

--- train_aggcn.sh
+++ train_aggcn.sh
@@ -6,14 +6,13 @@
 
 
 
-module load python/3.9 scipy-stack
+module load python scipy-stack
 virtualenv --no-download ~/scratch/AGGCN/semeval/env
 source ~/scratch/AGGCN/semeval/env/bin/activate
 pip install torch --no-index
 pip install tqdm
 
-SAVE_ID=$1
-python3 train.py --id $SAVE_ID --seed 0 --hidden_dim 300 --lr 0.7 --rnn_hidden 300 --num_epoch 100 --pooling max  --mlp_layers 1 --num_layers 2 --pooling_l2 0.002
+python train.py --id 1 --seed 0 --hidden_dim 300 --lr 0.7 --rnn_hidden 300 --num_epoch 100 --pooling max  --mlp_layers 1 --num_layers 2 --pooling_l2 0.002
 
-#python3 eval.py saved_models/01 --dataset test
+#python eval.py saved_models/01 --dataset test
 
