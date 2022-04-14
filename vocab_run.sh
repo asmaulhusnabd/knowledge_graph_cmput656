8e86d45f256ceb44ef3c33bca234c9d51eb7d566
asmaulhusnabd <tithi_cse_ruet@yahoo.com>
2022-04-13 22:54:15 -0600

    Update vocab_run.sh

--- vocab_run.sh
+++ vocab_run.sh
@@ -3,6 +3,6 @@
 #SBATCH --cpus-per-task=2
 #SBATCH --time=0-2:00
 
-module load python/3.9 scipy-stack
-python3 prepare_vocab.py dataset/tacred dataset/vocab --glove_dir dataset/glove
+module load python scipy-stack
+python prepare_vocab.py dataset/tacred dataset/vocab --glove_dir dataset/glove
 ~ 
